bc77de60e655b381f1282214272230b474a2ea09
124c4a11 <124c4a11@mail.ru>
2019-02-15 09:00:12 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app" class="container mx-auto py-6">
+    <header>
+      <div>
+        <span id="logo">Liquid Gold Box</span>
+      </div>
+    </header>
+
+    <div class="content"></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
